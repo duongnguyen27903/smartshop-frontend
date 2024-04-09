3a1bb274c07fbe7dfef64a06854e84d3f8acf994
duongnguyen27903 <100861773+duongnguyen27903@users.noreply.github.com>
2024-04-09 20:23:17 +0700

    udpate: css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        light: "wiggle 3s linear infinite",
+        highlight: "smash 1s ease-in infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        smash: {
+          "0%": { color: "yellow" },
+          // "50%": { transform: "scale(1.25,1.25)", color: "red" },
+          "100%": { transform: "scale(1.5,1.5)", color: "violet" },
+        },
+      },
+    },
   },
   plugins: [],
 };
